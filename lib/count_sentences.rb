371fea69fb24f41c9dc9bacf355b0afbcd8ece1e
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-15 20:11:57 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+  self.end_with?('.')
+
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    counter = 0
+    ary = self.split() 
+    ary.each do |ele|
+     if ele.sentence? || ele.exclamation? || ele.question? 
+      counter += 1
+  end 
+end
+  return counter
   end
 end
+
+
